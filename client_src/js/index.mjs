5458d581e5d7848fbb7f0814d9a578fcf5a14b49
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2019-04-14 22:09:33 +0100

    Tweak tree-shaking anchor

--- client_src/js/index.mjs
+++ client_src/js/index.mjs
@@ -12,9 +12,10 @@ import 'iso8601-js-period';
 window.addEventListener("load", function(_event) {
 	window.map_manager = new MapManager();
 	window.map_manager.setup();
-	
-	console.log("iso8601-js-period namespace: ", window.nezasa);
-	
-	// Ensure terser doesn't tree-shake  iso8601-js-period
-	console.debug(`iso8601-js-period ${nezasa.iso8601.Period?"is":"is not"} loaded.`);
 });
+
+// This needs to be here rather than in onload, because we need to ensure it's loaded now
+console.log("iso8601-js-period namespace: ", window.nezasa);
+
+// Ensure terser doesn't tree-shake  iso8601-js-period
+console.debug(`iso8601-js-period ${nezasa.iso8601.Period?"is":"is not"} loaded.`);
