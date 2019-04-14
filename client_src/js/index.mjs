4971d641e7aecbddad327336067587c69f06e7ca
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2019-04-14 22:23:32 +0100

    Strengthen iso8601-js-period tree-shaking anchor

--- client_src/js/index.mjs
+++ client_src/js/index.mjs
@@ -7,12 +7,16 @@ import '../../node_modules/leaflet-timedimension/dist/leaflet.timedimension.cont
 
 import MapManager from './MapManager.mjs';
 
-import 'iso8601-js-period';
+import iso8601 from 'iso8601-js-period';
 
 window.addEventListener("load", function(_event) {
 	window.map_manager = new MapManager();
 	window.map_manager.setup();
 });
+
+window.nezasa = {
+	iso8601
+};
 
 // This needs to be here rather than in onload, because we need to ensure it's loaded now
 console.log("iso8601-js-period namespace: ", window.nezasa);
