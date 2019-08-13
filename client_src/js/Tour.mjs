f3a7d8f76689b85b712be226964d96570d8ef40f
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2019-08-13 15:05:21 +0100

    Make remaining whitespace changes

--- client_src/js/Tour.mjs
+++ client_src/js/Tour.mjs
@@ -7,7 +7,7 @@ class Tour {
 	constructor(in_map_manager) {
 		this.map_manager = in_map_manager;
 	}
-
+	
 	create_tour() {
 		this.tour = new Sheperd.Tour({
 			useModalOverlay: true,
@@ -15,12 +15,12 @@ class Tour {
 				// Default settings for steps go here
 			}
 		});
-
+		
 		this.tour.addStep("welcome", {
 			text: "Welcome to the air quality web interface! Press next to get a short tour.",
 			buttons: this.get_buttons(true, false)
 		});
-
+		
 		this.tour.addStep("map", {
 			text: "Each device has a blue marker. The shape a marker is located in changes colour depending on the value of the measure it reported.",
 			attachTo: {
@@ -37,7 +37,7 @@ class Tour {
 			},
 			buttons: this.get_buttons()
 		});
-
+		
 		this.tour.addStep("reading-type", {
 			text: "Devices report multiple types of measurement. <strong>Change to another reading type now.</strong>",
 			attachTo: {
@@ -60,7 +60,7 @@ class Tour {
 			},
 			buttons: this.get_buttons()
 		});
-
+	
 		this.tour.addStep("map-controls", {
 			text: "You can control the zoom level and go fullscreen here. You can also zoom with your mouse wheel if you have one, and pan by clicking and dragging.",
 			attachTo: {
@@ -69,7 +69,7 @@ class Tour {
 			},
 			buttons: this.get_buttons()
 		});
-
+		
 		this.tour.addStep("device-graph", {
 			text: "By clicking a blue marker, you can view additional information about that device. Not all device are actively reporting data. <strong>Try clicking one now.</strong>",
 			attachTo: {
@@ -77,11 +77,11 @@ class Tour {
 				on: "top"
 			},
 			buttons: this.get_buttons(false)
-
+			
 		}).on("show", (() => {
 			this.map_manager.device_markers.once("marker-popup-opened", this.tour.next.bind(this.tour));
 		}).bind(this));
-
+		
 		this.tour.addStep("device-graph-a", {
 			text: "This is a device information popup. By default it displays recent data that the device has reported on the line graph, but you can control this with the blue buttons.",
 			attachTo: {
@@ -99,8 +99,8 @@ class Tour {
 			advanceOn: { selector: ".tabs :first-child a", event: "click" },
 			buttons: this.get_buttons(false)
 		});
-
-
+	
+	
 		this.tour.addStep("report-bug", {
 			text: "If you find a bug, you can report it by clicking this button.",
 			attachTo: {
@@ -117,32 +117,32 @@ class Tour {
 			},
 			buttons: this.get_buttons()
 		});
-
-
+	
+	
 		this.tour.addStep("complete", {
 			text: "Tour complete!\nIf you need any additional assistance, let us know :-)",
 			buttons: this.get_buttons(false, true, true)
 		});
 	}
-
+	
 	run_once() {
 		if(window.localStorage.getItem("completed_tour") === null)
 			this.run();
-
+		
 		//window.localStorage.setItem("completed_tour", (new Date()).toISOString()); -- removed due to an accidental refresh possiblity
 	}
-
+	
 	run() {
 		if(typeof this.tour == "undefined")
 			this.create_tour();
 		this.tour.start();
 	}
-
+	
 	completed_tour()
 	{
 		window.localStorage.setItem("completed_tour", (new Date()).toISOString());
 	}
-
+	
 	get_buttons(isContinue = true, isPrev = true, isEnd = false) {
 		let next = { text: "Next", action: this.tour.next },
 			prev = { text: "Previous", action: this.tour.back },
